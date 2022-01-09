ecadf88ceae75bb86181126ebd4b6e61d72169f3
mbimbij <joseph.mbimbi@gmail.com>
2022-01-09 10:50:59 +0100

    Apply formatter project wide

--- src/main/java/io/reflectoring/buckpal/common/SelfValidating.java
+++ src/main/java/io/reflectoring/buckpal/common/SelfValidating.java
@@ -9,21 +9,21 @@ import java.util.Set;
 
 public abstract class SelfValidating<T> {
 
-  private Validator validator;
+    private Validator validator;
 
-  public SelfValidating() {
-    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-    validator = factory.getValidator();
-  }
-
-  /**
-   * Evaluates all Bean Validations on the attributes of this
-   * instance.
-   */
-  protected void validateSelf() {
-    Set<ConstraintViolation<T>> violations = validator.validate((T) this);
-    if (!violations.isEmpty()) {
-      throw new ConstraintViolationException(violations);
+    public SelfValidating() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
-  }
+
+    /**
+     * Evaluates all Bean Validations on the attributes of this
+     * instance.
+     */
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
 }
